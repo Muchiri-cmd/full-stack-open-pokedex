0af0c67fce69303fd7a168bbab94b4600089e261
Muchiri-cmd <davismuchiri21@gmail.com>
2025-02-23 18:53:52 +0300

    Fix Tests workflows

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-// app.get('/health', (req, res) => {
-//   res.send('ok')
-// })
-
 app.get('/health', (req, res) => {
-  // eslint-disable-next-line no-constant-condition
-  if (true) throw('error...  ')
   res.send('ok')
 })
+
+// app.get('/health', (req, res) => {
+//   // eslint-disable-next-line no-constant-condition
+//   if (true) throw('error...  ')
+//   res.send('ok')
+// })
 
 const server = app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`)
